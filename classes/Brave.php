92fab5636a4129db9b4f3db117d5dacd15cb9ed2
EC2 Default User <ec2-user@ip-172-31-3-50.ap-southeast-2.compute.internal>
2024-12-27 09:58:47 +0000

    #6 カプセル化

--- classes/Brave.php
+++ classes/Brave.php
@@ -4,8 +4,15 @@ class Brave extends Human
 {
     
     const MAX_HITPOINT = 120;
-    public $hitPoint = self::MAX_HITPOINT;
-    public $attackPoint = 30;
+    //$nameには名前が一度設定されたら、その後変更できないような仕様にする
+    //publicだと、どこからでもアクセスできてしまい、意図しないところで変更されてしまう
+    //privateにして、アクセスの範囲を狭めて防ぐ
+    private $hitPoint = self::MAX_HITPOINT;
+    private $attackPoint = 30;
+    
+    //カプセル化のメリット:
+    //publicによってカプセル化される。
+    //カプセル化をすることで外部からのアクセスが可能になる。
     
     public function doAttack($enemy)
     {
