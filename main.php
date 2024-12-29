7a2c94b850f9023188620047ed7884a7079e52ec
EC2 Default User <ec2-user@ip-172-31-3-50.ap-southeast-2.compute.internal>
2024-12-29 13:05:23 +0000

    #10 リファクタリング前編

--- main.php
+++ main.php
@@ -6,6 +6,7 @@ require_once('./classes/Enemy.php');
 require_once('./classes/Brave.php');
 require_once('./classes/BlackMage.php');
 require_once('./classes/WhiteMage.php');
+require_once('./classes/Message.php');
 
 //インスタンス化(Humanクラス、Enemyクラスを使って人間、敵キャラを作る)
 //Humanクラスのコピーが$tiidaに入るイメージ
@@ -29,23 +30,31 @@ $turn = 1;
 
 $isFinishFlg = false;
 
+$messageObj = new Message;
+
 //どちらかのHPが0になるまで繰り返す
 while (!$isFinishFlg) {
     echo "*** $turn ターン目 ***\n\n";
+    
+    //仲間の表示
+    $messageObj->displayStatusMessage($members);
+    
+    //敵の表示
+    $messageObj->displayStatusMessage($enemies);
     
     //現在のHPの表示
     //constで定義した物(オブジェクト定数)は->ではなく、::で参照する
     //例: $goblin::MAX_HITPOINT
     //  : $tiida ::MAX_HITPOINT
     
-    foreach ($members as $member){
-        echo $member->getName() . " : ".$member->getHitPoint()."/".$member::MAX_HITPOINT."\n";
-    }
-    echo "\n";
-    foreach ($enemies as $enemy) {
-        echo $enemy->getName()." : ".$enemy->getHitPoint()."/".$enemy::MAX_HITPOINT."\n";
-    }
-    echo "\n";
+    // foreach ($members as $member){
+    //     echo $member->getName() . " : ".$member->getHitPoint()."/".$member::MAX_HITPOINT."\n";
+    // }
+    // echo "\n";
+    // foreach ($enemies as $enemy) {
+    //     echo $enemy->getName()." : ".$enemy->getHitPoint()."/".$enemy::MAX_HITPOINT."\n";
+    // }
+    // echo "\n";
     
     //攻撃 doAttackメソッドを呼ぶ
     foreach ($members as $member) {
@@ -103,10 +112,17 @@ while (!$isFinishFlg) {
 }
 
 echo "★★★ 戦闘終了 ★★★\n\n";
-foreach ($members as $member) {
-    echo $member->getName() . "　：　" . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
- }
- echo "\n";
-foreach ($enemies as $enemy) {
-    echo $enemy->getName() . "　：　" . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
- }
+
+//仲間の表示
+$messageObj->displayStatusMessage($members);
+
+//敵の表示
+$messageObj->displayStatusMessage($enemies);
+
+// foreach ($members as $member) {
+//     echo $member->getName() . "　：　" . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+//  }
+//  echo "\n";
+// foreach ($enemies as $enemy) {
+//     echo $enemy->getName() . "　：　" . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+//  }
